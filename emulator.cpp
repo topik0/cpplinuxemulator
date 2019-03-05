efc402835b7b516e3fdd66d8236032b2e3655b73
Topik <topik@topik.tech>
2019-03-04 18:18:30 -0600

    Create emulator.cpp

---
+++ emulator.cpp
@@ -0,0 +1,75 @@
+// UnixEmulator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+string hostname;
+string username;
+string input;
+string sudopassword;
+
+void loginStart();
+
+int prompt()
+{
+	cout << username << "@" << hostname << ":~$ ";
+	cin >> input;
+	if (input == "help") {
+		cout << "Help:" << endl;
+		cout << "help  -  Displays the help command" << endl;
+		cout << "logout  -  Ends the user session" << endl;
+		cout << "login  -  Creates a new user session" << endl;
+		prompt();
+	}
+	if (input == "shutdown") {
+		return 0;
+	}
+	if (input == "logout") {
+		cout << "Logging out..." << endl;
+		cout << "You have been logged out.  Type 'login' to login" << endl << username << "@" << hostname << ":~$ ";
+		loginStart();
+	}
+	if (input == "sudo -s") {
+		cout << "[sudo] password for " << username;
+
+	}
+	else {
+		cout << "-bash: " << input << ": " << "command not found" << endl;
+		prompt();
+	}
+}
+
+void login() {
+	cout << "Please choose a host name" << endl << "~$ ";
+	cin >> hostname;
+	cout << "Please choose a username" << endl << "~$ ";
+	cin >> username;
+	prompt();
+}
+
+void nonExistentCommandLogin() {
+	cout << "-bash: " << input << ": " << "command not found" << endl;
+	loginStart();
+}
+
+void loginStart() {
+	cin >> input;
+	if (input == "login") {
+		login();
+	}
+	else {
+		nonExistentCommandLogin();
+	}
+}
+
+int main()
+{
+	cout << "Welcome to the Unix terminal emulator made by Topik!" << "  " << "Please choose a host name" << endl << "~$ ";
+	cin >> hostname;
+	cout << "Please choose a username" << endl << "~$ ";
+	cin >> username;
+	prompt();
+}
