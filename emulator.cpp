4574fc7bf9ed84909f2333c3ca764edc4ad37cc3
Topik <topik@topik.tech>
2019-03-05 20:06:48 -0600

    Update emulator.cpp

--- emulator.cpp
+++ emulator.cpp
@@ -1,7 +1,7 @@
 // UnixEmulator.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-// #include "pch.h"
+#include "pch.h"
 #include <iostream>
 #include <string>
 using namespace std;
